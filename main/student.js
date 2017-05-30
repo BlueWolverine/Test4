265dc720adb3472e77c1c467995b8684a71b9f3e
LiMiaoJing <466020917@qq.com>
2017-05-30 23:15:37 +0800

    class

--- main/student.js
+++ main/student.js
@@ -1,3 +1,13 @@
+"use strict"
 let Person = require('./person')
 // Write your code here
+class Student extends Person{
+  constructor(name, age, kclass){
+    super(name,age)
+    this.class = kclass
+  }
+  introduce(){
+    return 'My name is ' + this.name + '. I am ' + this.age+' years old. I am a Student. I am at Class ' + this.class + '.'
+  }
+}
 module.exports = Student
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,11 +4,13 @@ const Student = require('../main/student');
 describe('main()', () => {
 
     it('should introduce person', () => {
+      "use strict"
       let person = new Person('Tom', 21);
       expect(person.introduce()).toBe("My name is Tom. I am 21 years old.");
     });
 
     it('should introduce student', () => {
+      "use strict"
       let student = new Student('Tom', 21, 2);
       expect(student.introduce()).toBe("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
     });
